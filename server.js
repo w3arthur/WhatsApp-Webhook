0e042bc218dfc711cf08854950b3d9d452dfdc02
Arthur Zarankin <78190695+w3arthur@users.noreply.github.com>
2023-02-15 18:44:20 +0200

    hgfhgfhf

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const PORT = process.env.PORT || 3500;
 //to set on .env        //require("dotenv").config();
 const MY_TOKEN = "arthur"; //token, verify the webhook     //to change
-const ACCESS_TOKEN = "1EAAM3hNlK66kBAB32x8FWM9DvZAdrvCo70GIFZBsIvc2OcebAQHqLxJBJw4zIOV1HaUGYWFeZBhR6nfEwUtXSIE7do7uzVxsSd2VfG5rZC6JoF5EIqmyLeJqSJAaCuClAyGrBGpykQFfC6aKLM9aujezXL80W4QEfbhRMe9MDCGaqGKHLy5vTgNZCZC6y7hL40NHTNg59QhswZDZD";
+const ACCESS_TOKEN = "EAAM3hNlK66kBAMhg2lLy39OcxIhyZBx63fwuHVCINKVvH7ahv4K9eN4dnJIHipNdCK5PAjnnLKY5gAZA5XYVbxSGsGMfEpYl0K23geQRv42h4gMcy4URyxTJcpDKjuLKp0oAHCB1HN8IZB0GPsyML21V7JpO2hQJrmeHeIDZCZAypZCPsrcWyJHJCQzpp1KbXUwYnZB7TQuZBwZDZD";
 //token, sending the request    //to change
 const mongoDBConnectionString = 'mongodb+srv://legopart:WfHIGKcxMGsllNS4@cluster0.uwlwx.mongodb.net/' + 'webhook';
 
@@ -80,7 +80,7 @@ app.route("/webhook")
             //^ take care!
             await axios({
                 method: 'POST'
-                , url: 'https://graph.facebook.com/v15.0/' + phone_number_id + '/messages?access_toke=' + ACCESS_TOKEN //116346754706966
+                , url: 'https://graph.facebook.com/v16.0/' + phone_number_id + '/messages?access_toke=' + ACCESS_TOKEN //116346754706966
                 , headers: {
                     Authorization: 'Bearer ' + ACCESS_TOKEN
                     , 'Content-Type': 'application/json'
